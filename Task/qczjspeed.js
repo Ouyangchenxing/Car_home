093296c3b66471e8b2d218c7f05cfb9f7fb0f9a4
ziye12 <59244461+ziye12@users.noreply.github.com>
2020-12-20 18:49:22 +0800

    汽车之家极速版  增加boxjs

--- Task/qczjspeed.js
+++ Task/qczjspeed.js
@@ -3,7 +3,7 @@
 github地址 https://github.com/ziye12/QCZJSPEED
 TG频道地址  https://t.me/ziyescript
 TG交流群   https://t.me/joinchat/AAAAAE7XHm-q1-7Np-tF3g
-boxjs链接  https://raw.githubusercontent.com/ziye12/QCZJSPEED/master/Task/ziye.boxjs.json
+boxjs链接  https://raw.githubusercontent.com/ziye12/QCZJSPEED/main/Task/ziye.qczjboxjs.json
 
 转载请备注个名字，谢谢
 
